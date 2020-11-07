49a899945f9cb999011f61a75030fd907dc35362
Kavin Tjhan <tjhan.kavin@gmail.com>
2020-11-07 13:24:29 -0800

    unfinished work

---
+++ src/component/Menu.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import '../index.css';
+import { Menu } from 'antd';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+
+const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+  ];
+  
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+
+class Sider extends React.Component {
+  handleClick = e => {
+    console.log('click ', e);
+  };
+
+  render() {
+    return (
+      <Menu
+        onClick={this.handleClick}
+        style={{ width: 256 }}
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode="inline"
+      >
+        <SubMenu
+          key="sub1"
+          title={
+            <span>
+              <MailOutlined />
+              <span>Navigation One</span>
+            </span>
+          }
+        >
+          <Menu.ItemGroup key="g1" title="Physicians">
+            <Menu.Item key="1">Hibbert, Julius</Menu.Item>
+            <Menu.Item key="2">Krieger, Algernop</Menu.Item>
+            <Menu.Item key="3">Riviera, Nick</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+      </Menu>
+    );
+  }
+}
+
+export default Sider;
